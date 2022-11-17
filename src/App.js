f339217e0821e89fa856e75a1abdddc2e08e4843
skrvasilis <skrvasilis@gmail.com>
2022-11-17 14:50:41 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+import useLocalStorageState from "use-local-storage-state";
 
-function App() {
+export default function App() {
+  /* const [imagesUrl, setImagesUrls] = useState([
+    "https://media.giphy.com/media/COYGe9rZvfiaQ/giphy.gif",
+    "https://media.giphy.com/media/I9BrUZDq5hksw/source.gif",
+    "https://media.giphy.com/media/B37cYPCruqwwg/source.gif",
+    "https://media.giphy.com/media/hHuOmWidPXwCk/source.gif",
+    "https://media.giphy.com/media/18hKuycmFuwaQ/source.gif",
+    "https://media.giphy.com/media/3oFzma9FGIblOf6Wk0/source.gif",
+    "https://media.giphy.com/media/7QxZfWnBLdGI8/source.gif",
+    "https://media.giphy.com/media/KmTnUKop0AfFm/source.gif",
+    "https://media.giphy.com/media/UseBZDm3O00hy/source.gif",
+    "https://media.giphy.com/media/l0HlMWkHJKvyjftKM/source.gif",
+    "https://media.giphy.com/media/d10dMmzqCYqQ0/source.gif",
+    "https://media.giphy.com/media/PzQvWAhgfUipW/source.gif",
+    "https://media.giphy.com/media/3q3RzbSNRugy0mH6LQ/giphy.gif",
+    "https://media.giphy.com/media/2gG2xiMTtFwsg/source.gif",
+    "https://media.giphy.com/media/YrD1PQldGsstG/source.gif",
+    "https://media.giphy.com/media/uHox9Jm5TyTPa/giphy.gif",
+    "https://media.giphy.com/media/gLQjUikb8nQnS/source.gif",
+    "https://media.giphy.com/media/B6G2MYBmtnGYU/source.gif",
+    "https://media.giphy.com/media/gE6IUBRWZd744/source.gif",
+    "https://media.giphy.com/media/cO39srN2EUIRaVqaVq/source.gif",
+  ]); */
+
+  const [imagesUrl, setImagesUrls] = useState([
+    "https://media.giphy.com/media/3xIErbNS4u2qIKW8tR/giphy.gif",
+    "https://media.giphy.com/media/YWW7VnJuHoCemgL2cx/giphy.gif",
+    "https://media.giphy.com/media/3o6ZsURHZbbjmbgBrO/giphy.gif",
+    "https://media.giphy.com/media/3o85xAevLNP7UQMco0/giphy.gif",
+    "https://media.giphy.com/media/l4FB8FICkBnuJnicU/giphy.gif",
+    "https://media.giphy.com/media/1iYG3wuv8ak6y8eI/giphy.gif",
+    "https://media.giphy.com/media/l46Cpa6DAjG2qSRLq/giphy.gif",
+    "https://media.giphy.com/media/TFzYO9DxRRFIc15lcI/giphy.gif",
+    "https://media.giphy.com/media/2sYKSeV8aBkdZuENXU/giphy.gif",
+    "https://media.giphy.com/media/l1EsZitDzerENKqpG/giphy.gif",
+    "https://media.giphy.com/media/1yTaZygj4gnWCUNDbC/giphy.gif",
+    "https://media.giphy.com/media/l3vRgcZnE2FYriCHu/giphy.gif",
+  "https://media.giphy.com/media/1dWSkX1bfXrVEBilY2/giphy.gif"
+  ]);
+
+  // const [students, setStudents] = useState([
+  //   "Aimee",
+  //   "Beda",
+  //   "Carlos",
+  //   "Christoph",
+  //   "Daniel",
+  //   "Francesco",
+  //   "Jonathan",
+  //   "Lucas",
+  //   "Lui",
+  //   "Martin",
+  //   "Norman",
+  //   "Priya",
+  //   "Habib",
+  //   "Shinhee",
+  //   "Shrekesh",
+  //   "Tim",
+  //   "Dalibor",
+  //   "Monishka",
+  // ]);
+
+  const [students, setStudents] = useState([
+    {name : "Aimee" , displayed: false},
+    {name : "Beda" , displayed: false},
+    {name : "Carlos" , displayed: false},
+    {name : "Christoph" , displayed: false},
+    {name : "Dalibor" , displayed: false},
+    {name : "Daniel" , displayed: false},
+    {name : "Francesco" , displayed: false},
+    {name : "Jonathan" , displayed: false},
+    {name : "Lucas" , displayed: false},
+    {name : "Lui" , displayed: false},
+    {name : "Martin" , displayed: false},
+    {name : "Monishka" , displayed: false},
+    {name : "Norman" , displayed: false},
+    {name : "Priya" , displayed: false},
+    {name : "Habid" , displayed: false},
+    {name : "Shinhee" , displayed: false},
+    {name : "Shrekesh" , displayed: false},
+    {name : "Tim" , displayed: false},
+    {name : "" , displayed: false},
+    {name : "" , displayed: false}
+  ]);
+
+  const [displayed, setDisplayed] = useState("");
+  const [imgdisplayed, setImgDisplayed] = useState("");
+  const [reducedStudents , setReducedStudents] = useState([])
+
+  const getRandomName = () => {
+    let randomNum = 0;
+    let i = 0;
+    let names = [...students];
+    let myInterval = setInterval(async () => {
+      randomNum = Math.floor(Math.random() * students.length);
+      i++;
+      setDisplayed(names[randomNum].name);
+
+      if (i === 17) {
+        clearInterval(myInterval);
+      }
+    }, 100);
+
+    const randomImg = Math.floor(Math.random() * imagesUrl.length);
+
+    setTimeout(() => {
+      setDisplayed(students[randomNum].name);
+      setImgDisplayed(imagesUrl[randomImg]);
+      const newStudents = students.filter((item) => item !== displayed);
+      setStudents(newStudents);
+    }, 1800);
+  };
+  console.log(students);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ul>
+        {students.map((item)=> {
+          return <li>{item.name}</li>
+        })}
+      </ul>
+      <button onClick={getRandomName}>go</button>
+      {displayed && <h1 className="name">{displayed}</h1>}
+      {imgdisplayed && <img src={imgdisplayed} className="image" />}
     </div>
   );
 }
-
-export default App;
---
+++ src/App2.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import "./App.scss";
+import useLocalStorageState from "use-local-storage-state";
+
+export default function App() {
+   const [imagesUrl, setImagesUrls] = useState([
+    "https://media.giphy.com/media/COYGe9rZvfiaQ/giphy.gif",
+    "https://media.giphy.com/media/I9BrUZDq5hksw/source.gif",
+    "https://media.giphy.com/media/B37cYPCruqwwg/source.gif",
+    "https://media.giphy.com/media/hHuOmWidPXwCk/source.gif",
+    "https://media.giphy.com/media/18hKuycmFuwaQ/source.gif",
+    "https://media.giphy.com/media/3oFzma9FGIblOf6Wk0/source.gif",
+    "https://media.giphy.com/media/7QxZfWnBLdGI8/source.gif",
+    "https://media.giphy.com/media/KmTnUKop0AfFm/source.gif",
+    "https://media.giphy.com/media/UseBZDm3O00hy/source.gif",
+    "https://media.giphy.com/media/l0HlMWkHJKvyjftKM/source.gif",
+    "https://media.giphy.com/media/d10dMmzqCYqQ0/source.gif",
+    "https://media.giphy.com/media/PzQvWAhgfUipW/source.gif",
+    "https://media.giphy.com/media/3q3RzbSNRugy0mH6LQ/giphy.gif",
+    "https://media.giphy.com/media/2gG2xiMTtFwsg/source.gif",
+    "https://media.giphy.com/media/YrD1PQldGsstG/source.gif",
+    "https://media.giphy.com/media/uHox9Jm5TyTPa/giphy.gif",
+    "https://media.giphy.com/media/gLQjUikb8nQnS/source.gif",
+    "https://media.giphy.com/media/B6G2MYBmtnGYU/source.gif",
+    "https://media.giphy.com/media/gE6IUBRWZd744/source.gif",
+    "https://media.giphy.com/media/cO39srN2EUIRaVqaVq/source.gif",
+  ]); 
+
+
+
+  const [students, setStudents] = useState([
+    {name : "Solomon" , displayed: false},
+    // {name : "Beda" , displayed: false},
+    {name : "Buddika" , displayed: false},
+    {name : "Elena" , displayed: false},
+    {name : "Iryna" , displayed: false},
+    {name : "Jime" , displayed: false},
+    {name : "Mads" , displayed: false},
+    {name : "Marwah" , displayed: false},
+    {name : "Mila" , displayed: false},
+    {name : "Mohamed" , displayed: false},
+    {name : "Moses" , displayed: false},
+    {name : "Netta" , displayed: false},
+    {name : "Peculiar" , displayed: false},
+    {name : "Ricardo" , displayed: false},
+    {name : "Rodrigue" , displayed: false},
+    {name : "Roxana" , displayed: false},
+    {name : "Sasha" , displayed: false},
+    {name : "Yassine" , displayed: false},
+    
+  ]);
+
+  const [displayed, setDisplayed] = useState("");
+  const [imgdisplayed, setImgDisplayed] = useState("");
+  const [reducedStudents , setReducedStudents] = useState([])
+
+  const getRandomName = () => {
+    let randomNum = 0;
+    let i = 0;
+    // let names = [...students];
+    let names = students.filter((item=>item.displayed === false))
+    console.log(names)
+    let myInterval = setInterval(async () => {
+      randomNum = Math.floor(Math.random() * names.length);
+      i++;
+      setDisplayed(names[randomNum].name);
+      setImgDisplayed(null)
+
+      if (i === 17) {
+        clearInterval(myInterval);
+      }
+    }, 100);
+
+    const randomImg = Math.floor(Math.random() * imagesUrl.length);
+
+    setTimeout(() => {
+      setDisplayed(names[randomNum].name);
+      setImgDisplayed(imagesUrl[randomImg]);
+      const newStudents = students.map((item) =>{
+          if (item.name === names[randomNum].name) {
+                item.displayed = true
+          }   return item     });
+      setStudents(newStudents);
+      console.log(newStudents)
+    }, 1800);
+  };
+//   console.log(students);
+
+
+
+
+
+  return (
+    <div className="container">
+      <ul>
+        {students.map((item)=> {
+          return <li className={item.displayed ? "displayed" : "null"}>{item.name}</li>
+        })}
+      </ul>
+      <button onClick={getRandomName}>go</button>
+      {displayed && <h1 className="name">{displayed}</h1>}
+      {imgdisplayed && <img src={imgdisplayed} className="image" />}
+  
+    </div>
+    
+  );
+}
